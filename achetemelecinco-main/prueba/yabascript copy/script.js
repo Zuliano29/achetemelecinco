66e169ec0569c505b25de4ed1e6ac5096ed06a54
Zuli <fernandofollatoros@gmail.com>
2025-09-24 19:34:28 +0200

    Add files via upload

---
+++ achetemelecinco-main/prueba/yabascript copy/script.js
@@ -0,0 +1,24 @@
+alert("¡Aqui tendras el sistema aritmetico mas complicado que podras encontrar, estas advertido¡");
+let nombre="Pepe";
+let num1=4;
+let num2=5;
+let resultado=(num1+num2);
+console.log(resultado);
+
+let num3=6;
+let num4=8;
+let resultado2=(num1-num2);
+console.log(resultado2);
+
+
+let num5=46;
+let num6=54;
+let resultado3=(num1*num2);
+console.log(resultado3);
+
+let num7=41;
+let num8=50;
+let resultado4=(num1/num2);
+console.log(resultado4);
+
+alert("Hola " + nombre);
